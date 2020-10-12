2dbc02fe34029ca87b319bb48feb09127228b8bb
Sriyoukan <36580905+Sriyoukan@users.noreply.github.com>
2020-10-12 09:36:39 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drag-and-drop';
+  files: File[] = [];
+  formData = new FormData();
+
+  
+
+  constructor(private http: HttpClient) { }
+
+
+
+  onSelect(event) {
+
+      console.log(event);
+
+      this.files.push(...event.addedFiles);
+
+
+
+      
+
+  
+
+      for (var i = 0; i < this.files.length; i++) { 
+
+        this.formData.append("file[]", this.files[i]);
+
+      }
+
+  }
+
+ sendToBackend(data){
+
+      this.http.post('endpointUrl', data)
+
+      .subscribe(res => {
+
+         console.log(res);
+
+         alert('Uploaded Successfully.');
+
+      })
+
+  }
+
+
+
+  onRemove(event) {
+
+      console.log(event);
+
+      this.files.splice(this.files.indexOf(event), 1);
+
+  }
+
 }
